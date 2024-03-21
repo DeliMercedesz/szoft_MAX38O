fdd893a8768a94a96de3566bd4bec924c25e73f6
DeliMercedesz <de.mercedesz@gmail.com>
2024-03-21 10:32:13 +0100

    elso

--- KigyosGame/Form1.cs
+++ KigyosGame/Form1.cs
@@ -2,24 +2,68 @@ namespace KigyosGame
 {
     public partial class Form1 : Form
     {
+        int fej_x = 100;
+        int fej_y = 100;
+        int irany_x = 1;
+        int irany_y = 0;
+        int lepesszam;
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            List<string> list = new List<string>();
+            KeyDown += Form1_KeyDown;
 
-            for (int i = 0; i < list.Count; i++)
+            foreach (KigyoElem item in Controls)
             {
-                rr
+                if(item.Top == fej_y && item.Left == fej_x)
+                {
+                    timer1.Enabled = false;
+                    return;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object? sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                irany_y = -1;
+                irany_x = 0;
             }
 
-            foreach(string item in list)
+            if(e.KeyCode == Keys.Down)
             {
-
+                irany_y = 1;
+                irany_x = 0;
             }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                irany_y = 0;
+                irany_x = -1;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                irany_y = 0;
+                irany_x = 1;
+            } 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lepesszam++;
+
+            fej_x += irany_x * KigyoElem.Meret;
+            fej_y += irany_y * KigyoElem.Meret;
+
+            KigyoElem ke = new KigyoElem();
+            ke.Top = fej_y;
+            ke.Left = fej_x;
+            Controls.Add(ke);
         }
     }
 }
---
+++ KigyosGame/KigyoElem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KigyosGame
+{
+    internal class KigyoElem : PictureBox
+    {
+        public static int Meret = 20;
+
+        public KigyoElem()
+        {
+            Width = KigyoElem.Meret;
+            Height = KigyoElem.Meret;
+            BackColor = Color.Fuchsia;
+        }
+    }
+}
