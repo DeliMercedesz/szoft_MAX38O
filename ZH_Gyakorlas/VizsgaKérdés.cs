15dafd47e59fdbe520e5f5327f699311c1819d18
DeliMercedesz <de.mercedesz@gmail.com>
2024-04-16 17:44:26 +0200

    .

---
+++ ZH_Gyakorlas/VizsgaKérdés.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZH_Gyakorlas
+{
+    public class VizsgaKérdés
+    {
+        public int Szam { get; set; }
+
+        public string Kerdes { get; set; } = string.Empty;
+
+        public string V1 { get; set; }=string.Empty;
+
+        public string V2 { get; set; } = string.Empty;
+
+        public string V3 { get; set; } = string.Empty;
+
+        public string Kep { get; set; } = string.Empty;
+
+        public int HelyesValasz { get; set; }
+       
+    }
+}
